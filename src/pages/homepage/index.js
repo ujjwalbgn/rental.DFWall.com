2d831ebe13805854ddb2ae854c042d9815bd1b97
Shrisan2 <shrisan2@live.com>
2024-01-18 21:08:58 -0600

    initial empty template

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,12 +1,3 @@
-import { motion } from "framer-motion";
-import ProductList from '../../components/products/productListing';
-
 export default function HomePage() {
-  return (
-     <section className="section-product-listing relative mt-28 max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto">
-     <div className="container-fluid relative md:mx-4 mx-2">
-       <ProductList/>
-     </div>
-   </section>
-  );
+  return <div className="container-fluid relative md:mx-4 mx-2 mt-40"></div>;
 }

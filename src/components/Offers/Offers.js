3b751ad3df614c3e59058c10d06e1c63daee7aa8
Shrish Dhakal <shrishd242@gmail.com>
2024-02-05 17:30:04 +0545

    second push of frontend

--- src/components/Offers/Offers.js
+++ src/components/Offers/Offers.js
@@ -7,7 +7,7 @@ import Offer from "../../../public/utilities/Offer.png"
 const Offers = () => {
   return (
     <div className="mt-10 mx-auto max-w-7xl p-8">
-      <h1 className="text-4xl font-medium mb-3 text-center ml-5 font-serif">Special Offers</h1>
+      <h1 className="text-4xl mb-3 text-center ml-5 ">Special Offers</h1>
       <section className="rounded-lg shadow-lg mx-auto max-w-md shadow-2xl rounded-lg">
       <Image src={Offer} className='w-[125px] h-[125px] ml-5'/>
         <h2 className="text-2xl font-medium mb-4 ml-5">Combination Offers</h2>
@@ -21,7 +21,7 @@ const Offers = () => {
           </p>
         </div>
 
-        <p className="font-serif text-center text-md pl-3 pr-3 pb-3">
+        <p className="text-center text-md pl-3 pr-3 pb-3">
           Elevate your experience with our exclusive combination offers. Limited time savings to make your gatherings extraordinary!
         </p>
       </section>
--- src/components/featured/featured.js
+++ src/components/featured/featured.js
@@ -4,23 +4,22 @@ import Bg from "../../../public/utilities/design.png";
 
 export default function Featured() {
   return (
-    <div className="relative flex flex-col items-center justify-center sm:flex-row">
+    <div className="flex items-center justify-center sm:flex-row sm:ml-20 ">
       {/* Hide the image on small screens (hidden on sm and smaller) */}
       <div className="hidden sm:block">
-        <Image src={Bg} className="inset-0 z-0 w-80 h-140" />
+        <Image src={Bg} className="inset-0 z-0 w-60 h-100" />
       </div>
-      <div className="relative z-10 text-center sm:text-left sm:ml-4">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium font-serif mb-2">
+      <div className="relative z-10 text-center sm:text-left sm:ml-4 w-full sm:w-3/5 mx-auto">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-medium font-serif mb-2">
           Experience The New Heights of Rental Service
         </h1>
-        <div className="w-full sm:w-[600px] mx-auto">
+        
           <p className="text-sm sm:text-base md:text-lg lg:text-lg mb-4 whitespace-normal">
             Discover comfort and style with our premium homely services. Choose
             from sleek folding chairs, elegant tables, and exciting games like
             cornhole, Jumbo Jenga, and Giant Connect Four.
           </p>
         </div>
-      </div>
     </div>
   );
 }
